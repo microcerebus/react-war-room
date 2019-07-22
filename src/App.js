7b68a22b5097c38a00370cd02aa163ed8363a218
Dhruv Jain <dhruv.jain.sg@gmail.com>
2019-07-22 23:36:46 +0800

    Edited App.js to display react war room across all pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 /* eslint-disable require-jsdoc */
 /* eslint-disable no-unused-vars */
 import React from 'react';
+import './App.css';
+
 import Home from './Home';
 
 function App() {
-  return <Home />;
+  return (
+    <div className="App mh4">
+      <header className="App-header tj mt0">
+        <h1>React War Room </h1>
+      </header>
+      <Home />
+    </div>
+  );
 }
 
 export default App;
